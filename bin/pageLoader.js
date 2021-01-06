bf6876e47adebf533b499143d2a6c8fdea13c8e4
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-01-06 20:38:32 +0300

    Add bin file

---
+++ bin/pageLoader.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import commander from 'commander';
+import loadPage from '../src/index.js';
+
+const { program } = commander;
+
+program
+  .version('1.0.0')
+  .description('Loads the page and it resources.')
+  .arguments('<pageUrl>')
+  .option('-o, --output [dir]', 'output dir', process.cwd())
+  .action((pageUrl, options) => loadPage(pageUrl, options.output)
+    .then((filepath) => console.log(filepath)));
+
+program.parse(process.argv);
