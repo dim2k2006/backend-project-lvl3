64e98ad125efee1ac9e3b7ddd0ea2c606a7a77c6
Dmitry Malofeev <dim2k2006@yandex.ru>
2021-01-16 13:20:38 +0300

    Update "loadPage" function
    Add support for loading images.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,13 +2,18 @@ import os from 'os';
 import path from 'path';
 import { promises as fs } from 'fs';
 import nock from 'nock';
-import { beforeEach, expect } from '@jest/globals';
+import { beforeEach, expect, jest } from '@jest/globals';
+import prettier from 'prettier';
+import cheerio from 'cheerio';
 import loadPage from '../src';
+
+jest.setTimeout(30000);
 
 nock.disableNetConnect();
 
 const getFixturePath = (filename) => path.join('.', '__fixtures__', filename);
 const readFile = (filename) => fs.readFile(filename, 'utf-8');
+const formatHtml = (htmlString) => prettier.format(htmlString, { parser: 'html' });
 
 let tempDir;
 
@@ -16,19 +21,63 @@ beforeEach(async () => {
   tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 
+const base = 'https://ru.hexlet.io';
+
 test('Loads the page.', async () => {
   const filename = 'ru-hexlet-io-courses.html';
   const expected = await readFile(getFixturePath(filename));
-  const base = 'https://ru.hexlet.io';
-  const endpoint = '/courses';
+  const pathname = '/courses';
 
   nock(base)
-    .get(endpoint)
+    .get(pathname)
     .reply(200, expected);
 
-  const filepath = await loadPage(`${base}${endpoint}`, tempDir);
+  const filepath = await loadPage(`${base}${pathname}`, tempDir);
 
   const actual = await readFile(filepath);
 
-  expect(actual).toBe(expected);
+  expect(actual).toBe(formatHtml(expected));
+});
+
+test('Loads the pages and all its images.', async () => {
+  const resourceName = 'ru-hexlet-io-courses';
+  const filesFolderName = `${resourceName}_files`;
+  const page = await readFile(getFixturePath('page-with-image.html'));
+  const expected = await readFile(getFixturePath('page-with-image-expected.html'));
+  const pathname = '/courses';
+  const imgPathname = '/assets/professions/nodejs.png';
+
+  nock(base)
+    .get(pathname)
+    .reply(200, page);
+
+  nock(base)
+    .get(imgPathname)
+    .replyWithFile(200, getFixturePath('nodejs.png'), {
+      'Content-Type': 'image/png',
+    });
+
+  const filepath = await loadPage(`${base}${pathname}`, tempDir);
+
+  const filesFolder = await fs.stat(path.join(tempDir, filesFolderName));
+
+  expect(filesFolder.isDirectory()).toBeTruthy();
+
+  const actual = await readFile(filepath);
+
+  expect(formatHtml(actual)).toBe(formatHtml(expected));
+
+  const $ = cheerio.load(actual);
+
+  const images = $('img');
+
+  images.each(async (idx, image) => {
+    const $image = $(image);
+
+    const source = $image.attr('src');
+
+    const imgFilePath = path.resolve(source);
+
+    expect(await fs.access(imgFilePath)).not.toThrow();
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 import axios from 'axios';
+import cheerio from 'cheerio';
+import prettier from 'prettier';
 
-const genFileName = (url) => {
-  const { hostname, pathname } = new URL(url);
-  const fileName = `${hostname}${pathname}`.replace(/[^a-zA-Z0-9]/g, '-');
+const genResourceName = (url) => url.replace(/[^a-zA-Z0-9]/g, '-');
 
-  return `${fileName}.html`;
+const loadPage = (url, dest = process.cwd()) => {
+  const resourceLink = new URL(url);
+  const base = `${resourceLink.protocol}//${resourceLink.hostname}`;
+
+  const resourceName = genResourceName(`${resourceLink.hostname}${resourceLink.pathname}`);
+  const loadedFilename = `${resourceName}.html`;
+  const loadedAssetsDirname = `${resourceName}_files`;
+  const loadedAssetsPath = path.join(dest, loadedAssetsDirname);
+  const destFilepath = path.join(dest, loadedFilename);
+
+  const loadImages = ({ data }) => {
+    const $ = cheerio.load(data);
+
+    const images = $('img');
+
+    const requests = images.map((idx, image) => {
+      const $image = $(image);
+      const imgUrl = new URL($image.attr('src'), base);
+      const filepath = path.parse(`${imgUrl.hostname}${imgUrl.pathname}`);
+
+      return new Promise((resolve, reject) => {
+        axios
+          .get(imgUrl.toString(), { responseType: 'arraybuffer' })
+          .then((response) => resolve({ ...response, filename: `${filepath.name}${filepath.ext}` }))
+          .catch((error) => reject(error));
+      });
+    });
+
+    images.each((idx, image) => {
+      const $image = $(image);
+
+      const imgUrl = new URL($image.attr('src'), base);
+      const filepath = path.parse(`${imgUrl.hostname}${imgUrl.pathname}`);
+      const newImgSource = genResourceName(`${filepath.dir}/${filepath.name}`);
+
+      $image.attr('src', `${loadedAssetsDirname}/${newImgSource}${filepath.ext}`);
+    });
+
+    const htmlString = $.html();
+
+    return Promise.all(requests)
+      .then((responses) => new Promise((resolve, reject) => {
+        return fs.mkdir(loadedAssetsPath)
+          .then(() => resolve(responses))
+          .catch((error) => reject(error));
+      }))
+      .then((responses) => {
+        const promises = responses.map((response) => {
+          const filepath = path.join(loadedAssetsPath, response.filename);
+          const buffer = response.data;
+
+          return fs.writeFile(filepath, buffer);
+        });
+
+        return Promise.all(promises);
+      })
+      .then(() => ({ data: htmlString }));
+  };
+
+  const promise = axios
+    .get(url)
+    .then(loadImages)
+    .then(({ data }) => fs.writeFile(destFilepath, prettier.format(data, { parser: 'html' }), 'utf-8'))
+    .then(() => destFilepath);
+
+  return promise;
 };
-
-const loadPage = (url, dest = process.cwd()) => axios
-  .get(url)
-  .then(({ data }) => fs.writeFile(path.join(dest, genFileName(url)), data, 'utf-8'))
-  .then(() => path.resolve(dest, genFileName(url)));
 
 export default loadPage;
